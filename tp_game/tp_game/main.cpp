c4a7f12d002960ac36b0c12e183d7b2a688e2720
Adriano J.P <adrianojpaulichi@outlook.com>
2019-03-26 14:35:50 -0300

    v0.1.0  + Print player texture  + Window view

--- tp_game/tp_game/main.cpp
+++ tp_game/tp_game/main.cpp
@@ -1,9 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+const float pixelScale(5.5f);
 
 int main()
 {
-	//Declara objeto janela
-	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML");
+	//Declare window and view
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Shield Hero", sf::Style::Close | sf::Style::Titlebar);
+	sf::View view(sf::Vector2f{ 0.0f, 0.0f }, (sf::Vector2f)window.getSize());
+	view.setSize( (sf::Vector2f) window.getSize()/pixelScale);
+	window.setView(view);
+
+	//Load player texture
+	sf::Texture texture;
+	if (!texture.loadFromFile("shield_hero.png")) 
+		{ std::cerr << "Couldn't load player texture! " << std::endl; }
+	//Player sprite
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setOrigin(sf::Vector2f{ texture.getSize() }/ 2.0f);
+	//Set sprite position
+	sprite.setPosition(sf::Vector2f{ 0.0f, 0.0f });
 
 	//Loop principal
 	while (window.isOpen())
@@ -20,6 +37,7 @@ int main()
 		}
 
 		window.clear(); //Limpa frame anterior
+		window.draw(sprite);
 		window.display(); //Mostra frame atual
 	}
 
